d5e11a7b26e073164f4fe75248c1fc92ba1a1157
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2022-10-17 14:07:11 -0500

    Initial Commit

---
+++ chullunquiaColecciondeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chullunquiaColecciondeJuegos
+//
+//  Created by MAC31 on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
